6d52fbe024ee64ae480e4765e8c8899fc661c792
dnwndalsrl <dnwndwlsqja@gmail.com>
2024-03-05 15:41:05 +0900

    프로젝트 셋팅

---
+++ src/views/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="temp">메인페이지임</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+div {
+  font-size: 30px;
+  div.temp {
+    color: red;
+  }
+}
+</style>
